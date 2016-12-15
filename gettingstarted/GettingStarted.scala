4bb54c840220a42c7c4dc560e0c81d41fdeed9b4
mdkn <m.kan0842024@gmail.com>
2016-12-15 21:30:09 +0900

    add factorial

--- gettingstarted/GettingStarted.scala
+++ gettingstarted/GettingStarted.scala
@@ -13,5 +13,21 @@ object MyModule {
 
   def main(args: Array[String]): Unit = {
     println(formatAbs(-42))
+    println(formatFactorial(7))
+  }
+
+  //recursive method go needs result type.
+  def factorial(n: Int): Int = {
+    @annotation.tailrec
+    def go(n: Int, acc: Int): Int = {
+      if(n <= 0) acc
+      else go(n - 1, n * acc)
+    }
+    go(n, 1)
+  }
+
+  private def formatFactorial(n: Int) = {
+    val msg = "The factorial value of %d is %d"
+    msg.format(n, factorial(n))
   }
 }
